ed6653caebfe0959d94c0b2c5aadcb734b2df98d
Jialucode <134432922+Jialucode@users.noreply.github.com>
2023-08-17 03:26:55 +0200

    1

--- app.py
+++ app.py
@@ -8,14 +8,14 @@ app=Flask(__name__)
 def add_chat():
     if request.method == "GET":
         #1.连接MySQL 
-        # conn = pymysql.connect(host="127.0.0.1",port=3306,user='root',passwd="123456",charset='utf8',db='userchats')
+        # conn = pymysql.connect(host="127.0.0.1",port=3306,user='root',passwd="123456",charset='utf8',db='userchats2')
         # cursor = conn.cursor(cursor=pymysql.cursors.DictCursor)
         # #2.发送指令
         # sql = "select * from admin"
         conn = pymysql.connect(host="containers-us-west-120.railway.app",port=7216,user='root',passwd="dW5oxfg0GdpkESgIbkDx",charset='utf8',db='railway')
         cursor = conn.cursor(cursor=pymysql.cursors.DictCursor)
         #2.发送指令
-        sql = "select * from userchats"
+        sql = "select * from userchats2"
         cursor.execute(sql)
         datalist = cursor.fetchall()
         print(datalist)
@@ -25,17 +25,17 @@ def add_chat():
         return render_template("index.html",data_list=datalist)
 
     user_input = request.form.get("input")
-    user_output = generate_text(user_input)
     print(user_input)
+    user_output = generate_text(user_input)
     # 连接mysql
 
     # #1.连接MySQL 
-    # conn = pymysql.connect(host="127.0.0.1",port=3306,user='root',passwd="123456",charset='utf8',db='userchats')
+    # conn = pymysql.connect(host="127.0.0.1",port=3306,user='root',passwd="123456",charset='utf8',db='userchats2')
     # cursor = conn.cursor(cursor=pymysql.cursors.DictCursor)
     conn = pymysql.connect(host="containers-us-west-120.railway.app",port=7216,user='root',passwd="dW5oxfg0GdpkESgIbkDx",charset='utf8',db='railway')
     cursor = conn.cursor(cursor=pymysql.cursors.DictCursor)
     #2.发送指令
-    sql = "insert into userchats(input,reply) values(%s,%s)"
+    sql = "insert into userchats2(input,reply) values(%s,%s)"
     #2.发送指令
     # sql = "insert into admin(input,reply) values(%s,%s)"
     cursor.execute(sql,[user_input,user_output])
@@ -44,14 +44,14 @@ def add_chat():
     cursor.close()
     conn.close()
     #1.连接MySQL 
-    # conn = pymysql.connect(host="127.0.0.1",port=3306,user='root',passwd="123456",charset='utf8',db='userchats')
+    # conn = pymysql.connect(host="127.0.0.1",port=3306,user='root',passwd="123456",charset='utf8',db='userchats2')
     # cursor = conn.cursor(cursor=pymysql.cursors.DictCursor)
     # #2.发送指令
     # sql = "select * from admin"
     conn = pymysql.connect(host="containers-us-west-120.railway.app",port=7216,user='root',passwd="dW5oxfg0GdpkESgIbkDx",charset='utf8',db='railway')
     cursor = conn.cursor(cursor=pymysql.cursors.DictCursor)
     #2.发送指令
-    sql = "select * from userchats"
+    sql = "select * from userchats2"
     cursor.execute(sql)
     datalist = cursor.fetchall()
     print(datalist)
@@ -69,11 +69,11 @@ def show_caht():
 
     # mysql -hcontainers-us-west-120.railway.app -uroot -pdW5oxfg0GdpkESgIbkDx --port 7216 --protocol=TCP railway
     #1.连接MySQL 
-    # conn = pymysql.connect(host="127.0.0.1",port=3306,user='root',passwd="123456",charset='utf8',db='userchats')
+    # conn = pymysql.connect(host="127.0.0.1",port=3306,user='root',passwd="123456",charset='utf8',db='userchats2')
     conn = pymysql.connect(host="containers-us-west-120.railway.app",port=7216,user='root',passwd="dW5oxfg0GdpkESgIbkDx",charset='utf8',db='railway')
     cursor = conn.cursor(cursor=pymysql.cursors.DictCursor)
     #2.发送指令
-    sql = "select * from userchats"
+    sql = "select * from userchats2"
     # sql = "select * from admin"
     cursor.execute(sql)
     datalist = cursor.fetchall()
